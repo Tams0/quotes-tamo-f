2e7569a91e8938b2a20787784f5440af747e1fff
Tams0 <126459373+Tams0@users.noreply.github.com>
2023-05-26 01:34:19 +0200

    display-count

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ def menu():
     print("1. Random quote")
     print("2. All quotes")
     print("3. Add quote")
-    print("4. Exit")
+    print("4. Display quotes")
+    print("5. Exit")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
+        choice = input("Choose your an action (1-5): ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
             view_quotes(quotes)
         elif choice == "3":
-            add_quote(quotes, input("Enter quote file: "))
+            add_quote(quotes, int(input("Enter quote file: ")))
         elif choice == "4":
+            display_quotes(quotes, input("Enter the number of quotes to display: "))
+        elif choice == "5":
             print("Good bye...")
             break
         else:
